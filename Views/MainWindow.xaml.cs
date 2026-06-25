097dd3cde133f3ac19b94f9634bca74b75a60a8a
LeoTao777 <1299689745@qq.com>
2026-06-25 20:59:44 +0800

    Refactor : using iNKORE.UI.WPF.Modern instead  iNKORE.UI.WPF.Modern

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using iNKORE.UI.WPF.Modern.Controls;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Page = iNKORE.UI.WPF.Modern.Controls.Page;
 
 namespace ShiJing.Views
 {
@@ -21,5 +13,47 @@ namespace ShiJing.Views
         {
             InitializeComponent();
         }
+
+        public SubPages.TopModules.HomePage HomePage = new SubPages.TopModules.HomePage();
+        public SubPages.TopModules.Video Video = new SubPages.TopModules.Video();
+        public SubPages.TopModules.Settings Settings = new SubPages.TopModules.Settings();
+        public SubPages.TopModules.About About = new SubPages.TopModules.About();
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var item = sender.SelectedItem;
+            Page? page = null;
+
+            if (item == NavigationViewItem_Home)
+            {
+                page = HomePage;
+            }
+            else if (item == NavigationViewItem_Video)
+            {
+                page = Video;
+            }
+            else if (item == NavigationViewItem_Settings)
+            {
+                page = Settings;
+            }
+            else if (item == NavigationViewItem_About)
+            {
+                page = About;
+            }
+
+            if (page != null)
+            {
+                NavigationView_Root.Header = page.Title;
+                Frame_Main.Navigate(page);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            NavigationView_Root.SelectedItem = NavigationViewItem_Home;
+        }
+
+
+
     }
 }
